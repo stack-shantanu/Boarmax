e73bb350bb254c63f274176f2f5797c4e39dbdef
kevingiulivostack <163465330+kevingiulivostack@users.noreply.github.com>
2024-03-14 17:10:43 -0400

    nice
    first commit

---
+++ force-app/main/default/lwc/b2Bcustombutton/b2Bcustombutton.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class CustomButton extends LightningElement {
+    handleClick() {
+        // Your click handler logic here
+        console.log('Button clicked');
+    }
+}
